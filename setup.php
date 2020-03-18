d0643dd5935f302b3b0baf72fbcd8a83b0d51230
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-03-18 22:52:47 +0700

    bugs: update typo

--- setup.php
+++ setup.php
@@ -1,4 +1,4 @@
-<? php 
+<?php 
     function pg_connection_string_from_database_url() {
         extract(parse_url($_ENV["DATABASE_URL"]));
         return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
