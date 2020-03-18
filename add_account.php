c4bd2e00afddff166b8cd7ae085896c7cc3f70c9
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-03-18 23:14:22 +0700

    fix typo again

--- add_account.php
+++ add_account.php
@@ -20,15 +20,15 @@
 
     function pg_connection_string_from_database_url() {
         extract(parse_url($_ENV["DATABASE_URL"]));
-        return "user=$user password=$password host=$host dbname=" . substr($path, 1);
-        # You may want to add sslmode=require there too 
+        return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
+        # <- You may want to add sslmode=require there too    
     }
 
     $db = pg_connect(pg_connection_string_from_database_url());
     if (!$db) {
         echo "Error: Unable to open database\n";
     } else {
-        echo "Openned database successfully \n";
+        echo "Opened database successfully\n";
     }
 
     $sql = "INSERT INTO MyAccounts (username, password) VALUES ('$username', '$password')";
--- list_account.php
+++ list_account.php
@@ -10,11 +10,11 @@
     <body>
         <?php
             function pg_connection_string_from_database_url() {
-                extract(parse_url($EVN["DATABASE_URL"]));
-                return "user=$user password=$password host=$host dbname=" . substr($path, 1);
-                # <- you may want to add sslmode=require there too
+                extract(parse_url($_ENV["DATABASE_URL"]));
+                return "user=$user password=$pass host=$host dbname=" . substr($path, 1); 
+                # <- You may want to add sslmode=require there too    
             }
-
+        
             $db = pg_connect(pg_connection_string_from_database_url());
             if (!$db) {
                 echo "Error: Unable to open database\n";
