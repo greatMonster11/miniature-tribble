c9f7346e327b9fe4b856051cb58c41c63afde95f
HAIDANG <dangtranhg@gmail.com>
2020-05-13 09:39:44 +0700

    add sytle for header

--- index.php
+++ index.php
@@ -1,9 +1,36 @@
 <html>
+<head>
+<sytle>
+a {
+  position: relative;
+  display: inline-block;
+   width: 150px;
+   text-decoration: none;
+   padding: 5px 10px;
+   background: black;
+   color: white; 
+   border: 4px solid mediumseagreen;
+   border-radius: 5px;
+   text-align: center;
+   font-weight: 600;
+   box-shadow: 1px 2px 2px #888888;
+}
+
+a:hover {
+  transform: translateY(2px);
+}
+
+a:active {
+  box-shadow: 1px 2px 2px black;
+}
+</style>
+</head>
 <body>
 <p>Le Quang Sang</p>
-<br /><a href="setup.php">Create database</a>
-<br /><a href="add_account.php">Add account</a>
-<br /><a href="list_account.php">List account</a>
+
+<a href="setup.php">Create database</a>
+<a href="add_account.php">Add account</a>
+<a href="list_account.php">List account</a>
 
 <form action="welcome.php" method="post">
 Ho va ten: <input type="text" name="name" id="name"><br />
