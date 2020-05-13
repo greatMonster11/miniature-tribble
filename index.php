14d737acca7939f4e02182ce0e1f5a7adcdf09dd
HAIDANG <dangtranhg@gmail.com>
2020-05-13 09:04:11 +0700

    update text value when page loaded

--- index.php
+++ index.php
@@ -6,10 +6,20 @@
 <br /><a href="list_account.php">List account</a>
 
 <form action="welcome.php" method="post">
-Ho va ten: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
+Ho va ten: <input type="text" name="name" id="name"><br />
+E-mail: <input type="text" name="email" id="email"><br />
+<input type="submit" />
 </form>
+
+<script>
+	window.onload = function() {
+		var name = document.getElementById("name")
+		var email = document.getElementById("email")
+
+		email.value = ""
+		name.value = ""
+	}
+</script>
 
 </body>
 </html>
